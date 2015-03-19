3537898615875d86ad347872f93027bf32425ab9
Bert Spaan <bert.spaan@gmail.com>
2015-03-19 09:53:56 +0100

    screen res message

--- js/street-patterns.jsx
+++ js/street-patterns.jsx
@@ -59,7 +59,7 @@ var StepIntro = React.createClass({
             <p>This is a tutorial on using <a href="http://wiki.openstreetmap.org/wiki/Key:highway">OpenStreetMap road data</a> to make <a href="http://dataphys.org/">physical visualizations</a> using <a href="http://fablab.waag.org/machines">Fablab equipment</a>. Follow along, and don&#39;t forget to click on all the links to read some background information!</p>
             <p>Beautiful patterns can emerge from a city&#39;s street network (<a href="http://www.fredfeddes.nl/">Fred Feddes</a> told us today that some of the patterns in the streets of Amsterdam are more than a 1000 years old), and by using only open data and open source tools, we can extract those patterns, and visualize and <i>physicalize</i> them.</p>
 
-            <p>This tutorial was made by <a href="http://bertspaan.nl">Bert Spaan</a> for the <a href="http://www.meetup.com/Maptime-AMS/events/220184211/">fifth edition</a> of <a href="http://maptime-ams.github.io/">Maptime Amsterdam</a>, and should work with Chrome, Firefox and Safari. The source code is available on <a href="https://github.com/maptime-ams/street-patterns">GitHub</a>.</p>
+            <p>This tutorial was made by <a href="http://bertspaan.nl">Bert Spaan</a> for the <a href="http://www.meetup.com/Maptime-AMS/events/220184211/">fifth edition</a> of <a href="http://maptime-ams.github.io/">Maptime Amsterdam</a>, and should work with Chrome, Firefox and Safari (and a screen resolution of at least 1280 by 800...). The source code is available on <a href="https://github.com/maptime-ams/street-patterns">GitHub</a>.</p>
             <p>On the next page, you can drag the map to find a street pattern you like, or search for a specific place.</p>
           </div>
         </div>
@@ -251,7 +251,9 @@ var StepGeoJSON = React.createClass({
   },
 
   onButtonClick: function() {
-    this.props.onNextStep({});
+    this.props.onNextStep({
+      geojson: this.editor.getValue()
+    });
   }
 });
 
